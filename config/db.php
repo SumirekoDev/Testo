2ada6271523eba26301a580fedd8b5e971f8d789
AdharaMatsu <cavazosmats27@gmail.com>
2021-12-02 00:58:33 -0600

    Pruebass

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli("localhost", "SumirekoDev", "pekora99", "estacionamiento");
+$db = new mysqli("localhost", "root", "", "estacionamiento");
 
 if ($db->connect_errno){
     echo "Fallo la conexion a la base de datos.";
